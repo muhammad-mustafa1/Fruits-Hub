5b606dfdfeee1f893dd812f8780bdef99706e403
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2025-01-27 11:06:11 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitSHub());
+}
+
+class FruitSHub extends StatelessWidget {
+  const FruitSHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
