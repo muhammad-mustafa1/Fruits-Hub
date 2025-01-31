66ac36d6e5c8007aad4be7ee703d0966f8f028f0
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2025-02-01 01:48:57 +0200

    use on generate route to navigate pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/helper_function/on_generate_routes.dart';
+import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 
 void main() {
-  runApp(const FruitSHub());
+  runApp(const FruitsHub());
 }
 
-class FruitSHub extends StatelessWidget {
-  const FruitSHub({super.key});
+class FruitsHub extends StatelessWidget {
+  const FruitsHub({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: SplashView.routeName,
+    );
   }
 }
