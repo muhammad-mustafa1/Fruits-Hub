92115e71d3ea8deb6707148a6813fb43e9aaf031
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2025-02-02 23:45:54 +0200

    add flutter_assets_gen to project and handel assets

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAppIcon
+  /// assets/images/app_icon.png
+  static const String imagesAppIcon = "assets/images/app_icon.png";
+
+  /// Assets for imagesCheckbox
+  /// assets/images/Checkbox.svg
+  static const String imagesCheckbox = "assets/images/Checkbox.svg";
+
+  /// Assets for imagesEllipse224
+  /// assets/images/Ellipse_224.svg
+  static const String imagesEllipse224 = "assets/images/Ellipse_224.svg";
+
+  /// Assets for imagesFruitBasketAmico1
+  /// assets/images/fruit_basket-amico_1.svg
+  static const String imagesFruitBasketAmico1 = "assets/images/fruit_basket-amico_1.svg";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.svg
+  static const String imagesLogo = "assets/images/logo.svg";
+
+  /// Assets for imagesPineappleCuate1
+  /// assets/images/pineapple_cuate_1.svg
+  static const String imagesPineappleCuate1 = "assets/images/pineapple_cuate_1.svg";
+
+  /// Assets for imagesPlant
+  /// assets/images/Plant.svg
+  static const String imagesPlant = "assets/images/Plant.svg";
+
+  /// Assets for imagesSplashBottom
+  /// assets/images/splash_bottom.svg
+  static const String imagesSplashBottom = "assets/images/splash_bottom.svg";
+
+  /// Assets for imagesVector
+  /// assets/images/Vector.svg
+  static const String imagesVector = "assets/images/Vector.svg";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector-1.svg
+  static const String imagesVector1 = "assets/images/Vector-1.svg";
+}
+
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/splash/presentation/views/widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
@@ -6,9 +7,7 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Fruits Hub'),
-      ),
+      body: SplashViewBody(),
     );
   }
 }
