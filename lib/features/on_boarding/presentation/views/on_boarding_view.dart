86349799af8a1ce6f071f50a7952f975f973903d
muhammad-mustafa1 <mohammedmustafa17798@gmail.com>
2025-02-17 10:33:19 +0200

    create OnBoardingView

---
+++ lib/features/on_boarding/presentation/views/on_boarding_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart';
+
+class OnBoardingView extends StatelessWidget {
+  const OnBoardingView({super.key});
+
+  static const String routeName = 'onBoardingView';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: const OnBoardingViewBody());
+  }
+}
